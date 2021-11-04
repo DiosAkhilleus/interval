2f581a51d6c534feb975471fc1245d7e888385c6
DiosAkhilleus <abertin1809@gmail.com>
2021-11-04 17:08:40 -0500

    added placeholder component for when user is logged out

---
+++ client/src/Components/PleaseLogIn.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+  
+}
+
+const PleaseLogIn = (props: Props) => {
+  return (
+    <div>
+      Please Log In
+    </div>
+  )
+}
+
+export default PleaseLogIn
