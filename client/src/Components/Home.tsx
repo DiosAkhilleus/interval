2d8a7912ef31e463ab26cfbe03dd09b12b93ed5a
DiosAkhilleus <abertin1809@gmail.com>
2021-11-04 17:08:00 -0500

    modified home layout

--- client/src/Components/Home.tsx
+++ client/src/Components/Home.tsx
@@ -1,30 +1,93 @@
 import React from 'react';
 import Loading from './Loading';
 import Post from './Post';
+import PleaseLogIn from './PleaseLogIn';
+import FriendActivityCard from './FriendActivityCard';
 import { useAuth0 } from '@auth0/auth0-react';
 interface Props {}
 
 const Home = (props: Props) => {
-  const { isLoading } = useAuth0();
+  const { isLoading, isAuthenticated } = useAuth0();
 
   if (isLoading) {
     return <Loading />;
   }
+  if (!isAuthenticated) {
+    return <PleaseLogIn />;
+  }
+  const posts = [
+    {
+      name: 'Jim Brown',
+      handle: '@jimbrown',
+      interval: 234,
+      profile_image: 'https://s.gravatar.com/avatar/c5aee30741780469d8b802522c6b550d?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fjb.png',
+      tags: ['#asdf', '#cheese', '#apples', '#coolstuff'],
+      text: 'I like running with the football very fast and jumping everywhere. I was a Cleveland Browns\' RB' 
+    },
+    {
+      name: 'John Doe',
+      handle: '@johndoe',
+      interval: 15,
+      profile_image: 'https://s.gravatar.com/avatar/c5aee30741780469d8b802522c6b550d?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fjd.png',
+      tags: ['#iam', '#john', '#doe'],
+      text: 'I am john doe what a dumb name that is very non-unique if you can believe it, so stupid placeholder name'
+    },
+    {
+      name: 'Baker Mayfield',
+      handle: '@bmayfield6',
+      interval: 543,
+      profile_image: 'https://s.gravatar.com/avatar/c5aee30741780469d8b802522c6b550d?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fbm.png',
+      tags: ['#futbol', '#football', '#sooners'],
+      text: 'OBJ is such a diva I wish he were off the team it\'s such a distraction to still have him on this Browns team'
+    },
+    {
+      name: 'Nick Chubb',
+      handle: '@nchubb27',
+      interval: 24,
+      profile_image: 'https://s.gravatar.com/avatar/c5aee30741780469d8b802522c6b550d?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fnc.png',
+      tags: ['#batman', '#super', '#strong'],
+      text: 'I like batman'
+    },
+  ];
+
+  const placeholderFriendActivity = [
+    {
+      handle: '@jimbrown', 
+      action: 'liked',
+      post_name: 'OBJ Sucks'
+    }, 
+    {
+      handle: '@bmayfield6',
+      action:'reposted',
+      post_name: 'Baker Mayfield is Good'
+    }, 
+    {
+      handle: '@nchubb27',
+      action: 'liked',
+      post_name: 'Batman Good'
+    }
+  ]
 
   return (
-    <div className='home-page-container'>
-      <div className='home-page-options'>
+    <div className="home-page-container">
+      <div className="home-page-options">
+        <div></div>
       </div>
-      <div className='home-page-posts'>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+      <div className="home-page-posts">
+        {posts.map((el, id) => (
+          <Post
+            handle={el.handle}
+            name={el.name}
+            interval={el.interval}
+            tags={el.tags}
+            profileImage={el.profile_image}
+            postText={el.text}
+          />
+        ))}
       </div>
-      <div className='home-trending-activity'>
+      <div className="home-friend-activity">
+        {placeholderFriendActivity.map((el, ind) => <FriendActivityCard handle={el.handle} action={el.action} postName={el.post_name} /> )}
+        
       </div>
     </div>
   );
