8c9520afa8a177e8fb9fa103a185e306980ccd47
DiosAkhilleus <abertin1809@gmail.com>
2021-11-11 15:40:02 -0600

    added tester graphql mutation component

---
+++ client/src/Components/PostUser.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useMutation } from '@apollo/client';
+import { POST_USER } from '../graphql/mutations';
+
+interface Props {
+  
+}
+const PostUser = (props: Props) => {
+
+  const info = {
+    profile_image: 'https://s.gravatar.com/avatar/c5aee30741780469d8b802522c6b550d?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fnc.png',
+    name: 'Nick Chubb',
+    public_handle: '@nchubb27', 
+    email: 'nchubb@yahoo.com',
+    followers: [],
+    following: [],
+    posts: []
+  }
+  const [postUser, { data, loading, error }] = useMutation(POST_USER)
+
+  if (loading) return <div>'Submitting'</div>
+  if (error) return <div>Error {error.message}</div>
+
+
+  return (
+    <div>
+      <button onClick={() => {
+        postUser({variables: info})
+      }}>Post User</button>
+    </div>
+  )
+}
+
+export default PostUser
