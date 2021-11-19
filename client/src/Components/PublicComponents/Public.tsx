d13984d4f7693d8927c22c7c99bbcb7a643c359d
DiosAkhilleus <abertin1809@gmail.com>
2021-11-19 10:05:25 -0600

    reorganized component file structure

---
+++ client/src/Components/PublicComponents/Public.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+  
+}
+
+const Public = (props: Props) => {
+  return (
+    <div>
+      Public Facing Page
+    </div>
+  )
+}
+
+export default Public
