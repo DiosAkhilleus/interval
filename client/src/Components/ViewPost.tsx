c4d16b56eefdf4e078aa2249998fa748bd4ba1c3
DiosAkhilleus <abertin1809@gmail.com>
2021-11-24 14:41:52 -0600

    made progress on post view page

--- client/src/Components/ViewPost.tsx
+++ client/src/Components/ViewPost.tsx
@@ -1,20 +1,48 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { GET_POST_BY_ID, GET_USER_BY_ID } from '../graphql/queries';
+import { useQuery } from '@apollo/client';
 
-interface Props {
-  
-}
+interface Props {}
 
 const ViewPost = (props: Props) => {
+  const { postId } = useParams<{ postId?: string }>();
+  const postFromId = useQuery(GET_POST_BY_ID, { variables: { id: postId } });
+  let history = useHistory();
 
-  const { postId }= useParams<{postId?: string}>();
+  const postCreator = useQuery(GET_USER_BY_ID, {
+    variables: { id: postFromId.data.getPostById[0].posted_by },
+  });
 
-
+  const handleGoBack = () => {
+    history.goBack();
+  };
 
   return (
-    <div>
-      { postId }
+    <div className="home-page-container">
+      <div className="post-page-card">
+        <div className="post-page-header">
+          <div
+            className="back-button"
+            onClick={() => {
+              handleGoBack();
+            }}
+          >
+            <img
+              src="https://img.icons8.com/ios/50/000000/circled-left-2.png"
+              alt="back-button"
+            />
+          </div>
+        </div>
+        {postFromId.data ? (
+          <div>{postFromId.data.getPostById[0].text}</div>
+        ) : (
+          ''
+        )}
+        {postCreator.data ? postCreator.data.getUserById[0].name : ''}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ViewPost
+export default ViewPost;
