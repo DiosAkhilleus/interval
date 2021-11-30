d18e3b579212ee10315421daed49923d2b475427
DiosAkhilleus <abertin1809@gmail.com>
2021-11-30 14:57:14 -0600

    modified layout

--- client/src/Components/ViewPost/PostReplyCard.tsx
+++ client/src/Components/ViewPost/PostReplyCard.tsx
@@ -37,7 +37,7 @@ const PostReplyCard = (props: Props) => {
       />
       <div className="post-page-reply">
         {postReply.data ? (
-          <ReplyCreator creatorID={postReply.data.getPostById[0].posted_by} />
+          <ReplyCreator replyInfo={postReply.data.getPostById[0]} />
         ) : (
           ''
         )}
