ca5493b4e0f0aaf818458214aeedd4ac69ba9ae2
DiosAkhilleus <abertin1809@gmail.com>
2021-11-30 14:57:05 -0600

    added reply count to display

--- client/src/Components/ViewPost/ReplyCreator.tsx
+++ client/src/Components/ViewPost/ReplyCreator.tsx
@@ -2,18 +2,28 @@ import React from 'react';
 import { GET_USER_BY_ID } from '../../graphql/queries';
 import { useQuery } from '@apollo/client';
 
+interface Reply {
+  posted_by: string
+  replies: Array<string>
+}
+
 interface Props {
-  creatorID: string;
+  replyInfo: Reply;
 }
 
 const ReplyCreator = (props: Props) => {
+
+  console.log(props.replyInfo)
+
   const replyCreator = useQuery(GET_USER_BY_ID, {
-    variables: { id: props.creatorID },
+    variables: { id: props.replyInfo.posted_by },
   });
 
   return (
-    <div style={{borderBottom: '2px solid rgb(8, 42, 52)'}}>
+    <div style={{borderBottom: '2px solid rgb(8, 42, 52)', display: 'flex', justifyContent: 'space-between'}}>
       <strong style={{fontSize: 24, marginLeft: 10}}>@{replyCreator.data ? replyCreator.data.getUserById[0].public_handle : ''}</strong>
+      <div style={{fontSize: 20, verticalAlign: 'middle', marginRight: 10, marginTop: 2}}>{props.replyInfo.replies.length ? props.replyInfo.replies.length === 1 ? `${props.replyInfo.replies.length} reply` : `${props.replyInfo.replies.length} replies` : '0 replies'}</div>
+
     </div>
   );
 };
