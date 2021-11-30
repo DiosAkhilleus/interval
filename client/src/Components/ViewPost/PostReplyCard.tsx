057a91f721ae6cc596f4f997010a691621df28c2
DiosAkhilleus <abertin1809@gmail.com>
2021-11-30 12:44:48 -0600

    added ability to reply to replies

--- client/src/Components/ViewPost/PostReplyCard.tsx
+++ client/src/Components/ViewPost/PostReplyCard.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
-import { GET_POST_BY_ID } from '../../graphql/queries';
+import { GET_POST_BY_ID, GET_USER_BY_ID } from '../../graphql/queries';
+import ReplyCreator from './ReplyCreator';
+import { Button } from 'react-bootstrap';
 
 interface Props {
   replyID: string;
@@ -8,11 +10,10 @@ interface Props {
 
 const PostReplyCard = (props: Props) => {
   // console.log(props.replyID);
-  const postReply = useQuery(GET_POST_BY_ID, { variables: { id: props.replyID } }); // Retrieves a post by its db _id
-  // useEffect(() => {
-  //   console.log(postReply);
-  // }, [postReply])
-
+  const postReply = useQuery(GET_POST_BY_ID, {
+    variables: { id: props.replyID },
+  }); // Retrieves a post by its db _id
+  // const replyPoster = useQuery(GET_USER_BY_ID, { variables: { id: postReply.data.}})
   return (
     <div
       style={{
@@ -24,12 +25,44 @@ const PostReplyCard = (props: Props) => {
       }}
     >
       <img
-        style={{ width: 40, height: 40, marginLeft: '3rem', marginTop: '0.5rem', transform: 'rotate(90deg) scale(1, -1)'}}
+        style={{
+          width: 40,
+          height: 40,
+          marginLeft: '3rem',
+          marginTop: '0.5rem',
+          transform: 'rotate(90deg) scale(1, -1)',
+        }}
         src="https://img.icons8.com/material/48/000000/reply-arrow--v1.png"
         alt="reply-arrow"
       />
       <div className="post-page-reply">
-        {postReply.data ? postReply.data.getPostById[0].text : ''}
+        {postReply.data ? (
+          <ReplyCreator creatorID={postReply.data.getPostById[0].posted_by} />
+        ) : (
+          ''
+        )}
+        <div style={{ margin: 10, fontSize: 18 }}>
+          {postReply.data ? postReply.data.getPostById[0].text : ''}
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+            flexDirection: 'row',
+          }}
+        >
+          {postReply.data ? (
+            <Button
+              style={{ marginRight: 10 }}
+              href={`/post/${postReply.data.getPostById[0].posted_by}/${props.replyID}`}
+              variant="outline-primary"
+            >
+              Reply to this comment
+            </Button>
+          ) : (
+            ''
+          )}
+        </div>
       </div>
     </div>
   );
