8ce561bdc94662af3e54a9d92c429cd6492ab59c
DiosAkhilleus <abertin1809@gmail.com>
2021-12-02 13:32:05 -0600

    added component for finding users

---
+++ client/src/Components/UserSearch.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+  
+}
+
+const UserSearch = (props: Props) => {
+  return (
+    <div style={{height: 100}}>
+      Search here for users
+    </div>
+  )
+}
+
+export default UserSearch
