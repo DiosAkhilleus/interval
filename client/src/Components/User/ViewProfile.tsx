37a7710a339d12ec6d171aa462736026810aa48a
DiosAkhilleus <abertin1809@gmail.com>
2021-12-02 13:33:31 -0600

    worked on profile page

--- client/src/Components/User/ViewProfile.tsx
+++ client/src/Components/User/ViewProfile.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { GET_USER_PROFILE } from '../../graphql/queries';
 
 interface Props {}
 
 const ViewProfile = (props: Props) => {
-  const { userHandle } = useParams<{ userHandle?: string }>();
+  const { userID } = useParams<{ userID?: string }>();
 
+  const userInfo = useQuery(GET_USER_PROFILE, {
+    // Retrieves the user who created the current post
+    variables: { id: userID },
+  });
 
-  return <div>{userHandle}</div>;
+  return <div>{userInfo.data ? userInfo.data.getUserById[0].name : ''}</div>;
 };
 
 export default ViewProfile;
