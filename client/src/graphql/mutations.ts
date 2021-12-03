632c4218dfcf098f4fc9eb873122a9d0d032ee76
DiosAkhilleus <abertin1809@gmail.com>
2021-12-03 13:47:54 -0600

    fixed gql client mutation

--- client/src/graphql/mutations.ts
+++ client/src/graphql/mutations.ts
@@ -2,7 +2,6 @@ import { gql } from '@apollo/client';
 
 // GraphQL Apollo mutations file
 
-
 export const MODIFY_USER_VOTE_FIELDS = gql`
   mutation ModifyUserVoteFields(
     $user_id: String!
@@ -10,49 +9,44 @@ export const MODIFY_USER_VOTE_FIELDS = gql`
     $type: String!
     $method: String!
   ) {
-    modifyUserVoteFields(user_id: $user_id, post_id: $post_id, type: $type, method: $method) {
+    modifyUserVoteFields(
+      user_id: $user_id
+      post_id: $post_id
+      type: $type
+      method: $method
+    ) {
       user_id
       post_id
       type
       method
     }
   }
-`
+`;
 
 export const MODIFY_POST_WITH_VOTE = gql`
-  mutation ModifyPostWithVote(
-    $id: String!
-    $type: String!
-    $method: String!
-  ) {
+  mutation ModifyPostWithVote($id: String!, $type: String!, $method: String!) {
     modifyPostWithVote(_id: $id, type: $type, method: $method) {
       type
       method
     }
   }
-`
+`;
 
 export const CHANGE_DISPLAY_NAME = gql`
-  mutation ChangeDisplayName(
-    $name: String!
-    $id: String!
-    ) {
-      changeName(name: $name, _id: $id) {
-        name
+  mutation ChangeDisplayName($name: String!, $id: String!) {
+    changeName(name: $name, _id: $id) {
+      name
     }
   }
 `;
 
 export const CHANGE_PUBLIC_HANDLE = gql`
-  mutation ChangePublicHandle(
-    $public_handle: String!
-    $id: String!
-  ) {
+  mutation ChangePublicHandle($public_handle: String!, $id: String!) {
     changeHandle(public_handle: $public_handle, _id: $id) {
       public_handle
     }
   }
-`
+`;
 
 export const POST_USER = gql`
   mutation PostUser(
@@ -85,31 +79,41 @@ export const POST_USER = gql`
 `;
 
 export const CREATE_POST = gql`
-  mutation CreatePost ($posted_at: String!, $posted_by: ID!, $repost_count: Number!, $reposted: Boolean!, $text: String!, $in_reply_to_public_handle: String! , $in_reply_to_user_id: String!, $likes: Number!, $dislikes: Number!, $user_mentions: [String], $tags: [String], $urls: [String] ) {
+  mutation CreatePost(
+    $posted_at: String!
+    $posted_by: ID!
+    $repost_count: Int!
+    $reposted: Boolean!
+    $reply: Boolean!
+    $title: String!
+    $text: String!
+    $in_reply_to_public_handle: String!
+    $in_reply_to_user_id: String!
+    $replies: [String!]!
+    $likes: Int!
+    $dislikes: Int!
+    $user_mentions: [String!]!
+    $tags: [String!]!
+    $urls: [String!]!
+  ) {
     createPost(
-      posted_at: $posted_at, 
-      posted_by: $posted_by,
-      repost_count: $repost_count,
-      reposted: $reposted,
-      text: $text,
-      in_reply_to_public_handle: $in_reply_to_public_handle,
-      likes: $likes,
-      dislikes: $dislikes,
+      posted_at: $posted_at
+      posted_by: $posted_by
+      repost_count: $repost_count
+      reposted: $reposted
+      reply: $reply
+      title: $title
+      text: $text
+      in_reply_to_public_handle: $in_reply_to_public_handle
+      in_reply_to_user_id: $in_reply_to_user_id
+      replies: $replies
+      likes: $likes
+      dislikes: $dislikes
       user_mentions: $user_mentions
       tags: $tags
       urls: $urls
     ) {
-      posted_at
-      posted_by
-      repost_count
-      reposted
-      text
-      in_reply_to_public_handle
-      likes
-      dislikes
-      user_mentions
-      tags
-      urls
+      title
     }
   }
 `;
