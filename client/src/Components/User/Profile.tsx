4da0d707e6897b7ecf8c690ea42297b8ffd676ea
DiosAkhilleus <abertin1809@gmail.com>
2021-12-03 13:48:25 -0600

    started formatting user profile component

--- client/src/Components/User/Profile.tsx
+++ client/src/Components/User/Profile.tsx
@@ -7,15 +7,17 @@ import { CHANGE_DISPLAY_NAME } from '../../graphql/mutations';
 
 interface Props {}
 
-const Profile = (props: Props) => { // User profile component for currently authenticated user
+const Profile = (props: Props) => {
+  // User profile component for currently authenticated user
   const { user } = useAuth0();
   const { email } = user!;
 
-  const currentUser = useQuery(GET_CURRENT_USER, { // Retrieves current user data from the db based on currently authenticated user
+  const currentUser = useQuery(GET_CURRENT_USER, {
+    // Retrieves current user data from the db based on currently authenticated user
     variables: { email: email },
   });
 
-  const [changeName, changeNameData] = useMutation(CHANGE_DISPLAY_NAME) // Modifies user display name based on input
+  const [changeName, changeNameData] = useMutation(CHANGE_DISPLAY_NAME); // Modifies user display name based on input
 
   console.log(changeNameData.data);
 
@@ -28,59 +30,73 @@ const Profile = (props: Props) => { // User profile component for currently auth
       setProfileName(currentUser.data.currentUser[0].name);
       setUserID(currentUser.data.currentUser[0].id);
     }
-  }, [currentUser])
-  
-  const handleNameEdit = () => { // Updates state to allow name editing
+  }, [currentUser]);
+
+  const handleNameEdit = () => {
+    // Updates state to allow name editing
     console.log(profileName);
     setEditName(true);
   };
 
-  const confirmNameChange = () => { // Posts new name to db
+  const confirmNameChange = () => {
+    // Posts new name to db
     console.log(profileName);
     setEditName(false);
-    changeName({variables: {name: profileName, id: userID}})
-  }
+    changeName({ variables: { name: profileName, id: userID } });
+  };
 
   return (
-    <div style={{ marginTop: 10 }}>
-      <div className="row align-items-center profile-header">
-        <div className="col-md-2 mb-3">
-          <img
-            src={currentUser.data ? currentUser.data.currentUser[0].profile_image : ''}
-            alt="Profile"
-            className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
-            style={{ width: 180 }}
-          />
-        </div>
-        <div className="col-md text-center text-md-left">
-          <div className="display-name-box">
-            {editName ? (
-              <div>
-                <input
-                  placeholder={profileName}
-                  type="text"
-                  onChange={(e) => setProfileName(e.target.value)}
-                ></input>
-                <button onClick={() => confirmNameChange()}>Confirm Name</button>
-              </div>
-            ) : (
-              <div>
-                <div>{profileName}</div>
-                <button onClick={() => handleNameEdit()}>
-                  Edit Display Name
-                </button>
-              </div>
-            )}
+    <div className="home-page-container">
+      <div className='post-page-card'>
+        <div className="row align-items-center profile-header">
+          <div className="col-md-2 mb-3">
+            <img
+              src={
+                currentUser.data
+                  ? currentUser.data.currentUser[0].profile_image
+                  : ''
+              }
+              alt="Profile"
+              className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
+              style={{ width: 180 }}
+            />
           </div>
+          <div className="col-md text-center text-md-left">
+            <div className="display-name-box">
+              {editName ? (
+                <div>
+                  <input
+                    placeholder={profileName}
+                    type="text"
+                    onChange={(e) => setProfileName(e.target.value)}
+                  ></input>
+                  <button onClick={() => confirmNameChange()}>
+                    Confirm Name
+                  </button>
+                </div>
+              ) : (
+                <div>
+                  <div>{profileName}</div>
+                  <button onClick={() => handleNameEdit()}>
+                    Edit Display Name
+                  </button>
+                </div>
+              )}
+            </div>
 
-          {currentUser.data ? <h2>@{currentUser.data.currentUser[0].public_handle}</h2> : ''}
-          <p className="lead text-muted">{email}</p>
+            {currentUser.data ? (
+              <h2>@{currentUser.data.currentUser[0].public_handle}</h2>
+            ) : (
+              ''
+            )}
+            <p className="lead text-muted">{email}</p>
+          </div>
         </div>
-      </div>
-      <div className="row">
-        <pre className="col-12 text-light bg-dark p-4">
-          {JSON.stringify(user, null, 2)}
-        </pre>
+        <div className="row">
+          <pre className="col-12 text-light bg-dark p-4">
+            {JSON.stringify(user, null, 2)}
+          </pre>
+        </div>
       </div>
     </div>
   );
