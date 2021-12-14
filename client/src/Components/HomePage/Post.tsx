6a99aea06f71b4c6eca7ff53d87772e582f6dbbf
DiosAkhilleus <abertin1809@gmail.com>
2021-12-14 10:54:38 -0600

    updated posts to link currently authenticated user's profile link to /profile

--- client/src/Components/HomePage/Post.tsx
+++ client/src/Components/HomePage/Post.tsx
@@ -29,25 +29,20 @@ interface Props {
   postInfo: PostInterface;
   currentUserLikedPosts: Array<string>;
   currentUserDislikedPosts: Array<string>;
-  currentUserId: String;
+  currentUserId: string;
 }
 
-const Post = ({
-  postInfo,
-  currentUserLikedPosts,
-  currentUserDislikedPosts,
-  currentUserId,
-}: Props) => {
+const Post = (props: Props) => {
   const { isLoading } = useAuth0();
   const [image, setImage] = useState('');
   const [likedByUser, setLikedByUser] = useState(false); // Is current post "liked" by user
   const [dislikedByUser, setDislikedByUser] = useState(false); // Is current post "disliked" by user
-  const [postLikes, setPostLikes] = useState(postInfo.likes); // How many likes does the current post have
-  const [postDislikes, setPostDislikes] = useState(postInfo.dislikes); // How many dislikes does the current post have
+  const [postLikes, setPostLikes] = useState(props.postInfo.likes); // How many likes does the current post have
+  const [postDislikes, setPostDislikes] = useState(props.postInfo.dislikes); // How many dislikes does the current post have
 
   const { data } = useQuery(GET_USER_BY_ID, {
     // Retrieves the user who created the current post
-    variables: { id: postInfo.posted_by },
+    variables: { id: props.postInfo.posted_by },
   });
 
   // eslint-disable-next-line
@@ -65,11 +60,11 @@ const Post = ({
     // Sets all the state variables depending on the information retrieved from the db
     if (data) {
       setImage(data.getUserByID[0].profile_image);
-      const filteredUserLikes = currentUserLikedPosts.filter(
-        (el: string) => el === postInfo.id
+      const filteredUserLikes = props.currentUserLikedPosts.filter(
+        (el: string) => el === props.postInfo.id
       );
-      const filteredUserDislikes = currentUserDislikedPosts.filter(
-        (el: string) => el === postInfo.id
+      const filteredUserDislikes = props.currentUserDislikedPosts.filter(
+        (el: string) => el === props.postInfo.id
       );
       if (filteredUserLikes.length > 0) {
         setLikedByUser(true);
@@ -77,58 +72,69 @@ const Post = ({
       if (filteredUserDislikes.length > 0) {
         setDislikedByUser(true);
       }
-    } 
+    }
     // eslint-disable-next-line
   }, [data]);
 
   const handleVote = (vote: string) => {
     // Handles a user's vote on a post
 
+    if (vote === 'like') {
+      // If the user "likes" the post...
 
-    if (vote === 'like') { // If the user "likes" the post...
-
-      if (likedByUser) {  // If the post has already been liked by the user...
+      if (likedByUser) {
+        // If the post has already been liked by the user...
         setLikedByUser(!likedByUser);
         setPostLikes(postLikes - 1);
         modifyPostWithVote({
-          variables: { id: postInfo.id, type: 'likes', method: 'decrement' },
+          variables: {
+            id: props.postInfo.id,
+            type: 'likes',
+            method: 'decrement',
+          },
         });
         modifyUserVoteFields({
           variables: {
-            user_id: currentUserId,
-            post_id: postInfo.id,
+            user_id: props.currentUserId,
+            post_id: props.postInfo.id,
             type: 'liked_posts',
             method: 'remove',
           },
         });
-      } else if (!likedByUser) { // If the post has not been liked by the user
+      } else if (!likedByUser) {
+        // If the post has not been liked by the user
         setLikedByUser(!likedByUser);
         setPostLikes(postLikes + 1);
         modifyPostWithVote({
-          variables: { id: postInfo.id, type: 'likes', method: 'increment' },
+          variables: {
+            id: props.postInfo.id,
+            type: 'likes',
+            method: 'increment',
+          },
         });
         modifyUserVoteFields({
           variables: {
-            user_id: currentUserId,
-            post_id: postInfo.id,
+            user_id: props.currentUserId,
+            post_id: props.postInfo.id,
             type: 'liked_posts',
             method: 'add',
           },
         });
-        if (dislikedByUser === true) { // If the post has already been "disliked" by the user before later being "liked"...
+        if (dislikedByUser === true) {
+          // If the post has already been "disliked" by the user before later being "liked"...
           setDislikedByUser(false);
           setPostDislikes(postDislikes - 1);
           modifyPostWithVote({
             variables: {
-              id: postInfo.id,
+              id: props.postInfo.id,
               type: 'dislikes',
               method: 'decrement',
             },
           });
           modifyUserVoteFields({
             variables: {
-              user_id: currentUserId,
-              post_id: postInfo.id,
+              user_id: props.currentUserId,
+              post_id: props.postInfo.id,
               type: 'disliked_posts',
               method: 'remove',
             },
@@ -136,47 +142,63 @@ const Post = ({
         }
       }
     }
-    
-    if (vote === 'dislike') { // If the user "dislikes" the post...
 
-      if (dislikedByUser) { // If the post has already been "disliked" by the user...
+    if (vote === 'dislike') {
+      // If the user "dislikes" the post...
+
+      if (dislikedByUser) {
+        // If the post has already been "disliked" by the user...
         setDislikedByUser(!dislikedByUser);
         setPostDislikes(postDislikes - 1);
         modifyPostWithVote({
-          variables: { id: postInfo.id, type: 'dislikes', method: 'decrement' },
+          variables: {
+            id: props.postInfo.id,
+            type: 'dislikes',
+            method: 'decrement',
+          },
         });
         modifyUserVoteFields({
           variables: {
-            user_id: currentUserId,
-            post_id: postInfo.id,
+            user_id: props.currentUserId,
+            post_id: props.postInfo.id,
             type: 'disliked_posts',
             method: 'remove',
           },
         });
-      } else if (!dislikedByUser) { // If the post has not already been "disliked" by the user...
+      } else if (!dislikedByUser) {
+        // If the post has not already been "disliked" by the user...
         setDislikedByUser(!dislikedByUser);
         setPostDislikes(postDislikes + 1);
         modifyPostWithVote({
-          variables: { id: postInfo.id, type: 'dislikes', method: 'increment' },
+          variables: {
+            id: props.postInfo.id,
+            type: 'dislikes',
+            method: 'increment',
+          },
         });
         modifyUserVoteFields({
           variables: {
-            user_id: currentUserId,
-            post_id: postInfo.id,
+            user_id: props.currentUserId,
+            post_id: props.postInfo.id,
             type: 'disliked_posts',
             method: 'add',
           },
         });
-        if (likedByUser === true) { // If the post was already "liked" by the user before later being "disliked"...
+        if (likedByUser === true) {
+          // If the post was already "liked" by the user before later being "disliked"...
           setPostLikes(postLikes - 1);
           setLikedByUser(false);
           modifyPostWithVote({
-            variables: { id: postInfo.id, type: 'likes', method: 'decrement' },
+            variables: {
+              id: props.postInfo.id,
+              type: 'likes',
+              method: 'decrement',
+            },
           });
           modifyUserVoteFields({
             variables: {
-              user_id: currentUserId,
-              post_id: postInfo.id,
+              user_id: props.currentUserId,
+              post_id: props.postInfo.id,
               type: 'liked_posts',
               method: 'remove',
             },
@@ -192,17 +214,29 @@ const Post = ({
         <div className="post-creator-details">
           <div className="post-creator-name-grouping">
             <h3 style={{ width: '100%', marginBottom: 4, marginLeft: 10 }}>
-              {postInfo.title}
+              {props.postInfo.title}
             </h3>
           </div>
           <div className="post-creator-handle">
             {data ? (
-              <div>
-                <strong>{data.getUserByID[0].name}</strong>
-                <Link style={{color: 'darkcyan'}} to={`/users/${postInfo.posted_by}`}>
-                  <div>@{data.getUserByID[0].public_handle}</div>
-                </Link>
-              </div>
+              props.currentUserId === props.postInfo.posted_by ? (
+                <div>
+                  <strong>{data.getUserByID[0].name}</strong>
+                  <Link style={{ color: 'darkcyan' }} to={'/profile'}>
+                    <div>@{data.getUserByID[0].public_handle}</div>
+                  </Link>
+                </div>
+              ) : (
+                <div>
+                  <strong>{data.getUserByID[0].name}</strong>
+                  <Link
+                    style={{ color: 'darkcyan' }}
+                    to={`/users/${props.postInfo.posted_by}`}
+                  >
+                    <div>@{data.getUserByID[0].public_handle}</div>
+                  </Link>
+                </div>
+              )
             ) : (
               ''
             )}
@@ -219,10 +253,10 @@ const Post = ({
           )}
         </div>
         <div className="post-content-details">
-          <div style={{ marginBottom: 15 }}>{postInfo.text}</div>
+          <div style={{ marginBottom: 15 }}>{props.postInfo.text}</div>
           <div className="reply-button-div">
             <Button
-              href={`/post/${postInfo.posted_by}/${postInfo.id}`}
+              href={`/post/${props.postInfo.posted_by}/${props.postInfo.id}`}
               variant="outline-primary"
             >
               See Post & Replies...
@@ -231,14 +265,16 @@ const Post = ({
         </div>
 
         <div className="post-content-tags">
-          {postInfo.entities.tags.map((el: string, id: number) => (
-            <div style={{ marginLeft: 10 }} key={id}>#{el}</div>
+          {props.postInfo.entities.tags.map((el: string, id: number) => (
+            <div style={{ marginLeft: 10 }} key={id}>
+              #{el}
+            </div>
           ))}
           <div style={{ width: '100%', textAlign: 'right', marginRight: 10 }}>
-            {postInfo.replies
-              ? postInfo.replies.length === 1
-                ? `${postInfo.replies.length} reply`
-                : `${postInfo.replies.length} replies`
+            {props.postInfo.replies
+              ? props.postInfo.replies.length === 1
+                ? `${props.postInfo.replies.length} reply`
+                : `${props.postInfo.replies.length} replies`
               : ''}{' '}
           </div>
         </div>
